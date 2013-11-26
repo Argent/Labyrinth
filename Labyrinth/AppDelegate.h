ebac4b594bc70beec13f54d2ffccbc9809588b31
Benjamin Otto <ben.otto2@gmail.com>
2013-11-26 12:22:59 +0100

    Initial Commit

---
+++ Labyrinth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Labyrinth
+//
+//  Created by Benjamin Otto on 26.11.13.
+//  Copyright (c) 2013 Benjamin Otto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
